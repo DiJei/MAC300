9523640a3b5dbab4df1cd5ab298dd03102f74679
Abello966 <antonio.augusto.abello@gmail.comcd>
2014-10-05 02:40:24 -0300

    erros na decomposição LU

--- ep1.c
+++ ep1.c
@@ -54,7 +54,7 @@ int main(int argc, char** argv) {
    
    
    /*Acha a decomposicao LU por coluna
-   e imprime*/
+   e imprime
    printf("Decomposicao LU coluna:\n");
    lucol(A,n,p);
    for(x = 0; x < n; x++) {
@@ -62,7 +62,7 @@ int main(int argc, char** argv) {
          printf("%f ",A[x][y]);
        printf("\n"); 
    }
-   /*Agora resolve por coluna e imprime*/
+   /*Agora resolve por coluna e imprime
    printf("Solucao Coluna: \n");
    sscol(n,A,p,b);
    for(x = 0; x < n; x++)
@@ -167,21 +167,22 @@ void trocaLinha(int n, int x, int m, double a[][nmax]) {
 int lucol(double a[][nmax], int n, int intch[])
 {
   int i, j, k, maxk;
-  for (k = 0; k < (n - 1); k++)/*ate (n-1)*/
+  for (k = 0; k < n; k++)
   {
     /*retorna índice do máximo*/
-    maxk = 0;
+    maxk = k;
     for (i = k; i < n; i++)/*antes: i = k + 1 agora i = k*/
-       if (modulo(a[i][k]) > a[maxk][k])
+    {
+       if (modulo(a[i][k]) > modulo(a[maxk][k]))
          maxk = i;
+    }
     
-    
-    if (a[k][maxk] = 0)
+    if (a[maxk][k] == 0)
       return -1; /*É singular*/ 
+	
     else
     {
       intch[k] = maxk; /*Guardamos a mudança de row*/
-
       if (maxk != k)
       {
          //trocar a linha a[k][] por a[maxk][]
@@ -191,12 +192,13 @@ int lucol(double a[][nmax], int n, int intch[])
       if (a[k][k] == 0)
          return (-1);
 
-      for (i = k + 1; i < n; i++) /*Calcula os multiplicadores*/
+      for (i = k + 1; i < n; i++)
+      { 
          a[i][k] = a[i][k] / a[k][k];
-
-      for (j = k + 1; i < n; i++) /*Subtrai a linha k a[i][k] vezes da linha i*/
-         for (i = k + 1; j < n; j++)
+         for (j = k + 1; j < n; j++)
             a[i][j] = a[i][j] - a[i][k] * a[k][j];
+      }
+      
     }
       
   }
@@ -210,13 +212,14 @@ int lurow(double a[][nmax], int n, int intch[])
   for (k = 0; k < n; k++)
   {
     /*retorna índice do máximo*/
-    maxk = 0;
-    for (i = k + 1; i < n; i++)
-       if (modulo(a[i][k]) > a[maxk][k])
+    maxk = k;
+    for (i = k; i < n; i++)
+    {
+      if (modulo(a[i][k]) > modulo(a[maxk][k]))
 	       maxk = i;
-    
+    }
 
-    if (a[k][maxk] = 0)
+    if (a[maxk][k] == 0)
       return -1; /*É singular*/ 
     else
     {
@@ -232,11 +235,11 @@ int lurow(double a[][nmax], int n, int intch[])
 	       return (-1);
 
       for (i = k + 1; i < n; i++) /*Calcula os multiplicadores*/
-	       a[i][k] = a[i][k] / a[k][k];
-
-      for (i = k + 1; i < n; i++) /*Subtrai a linha k a[i][k] vezes da linha i*/
-	       for (j = k + 1; j < n; j++)
-	          a[i][j] = a[i][j] - a[i][k] * a[k][j];
+      {
+	a[i][k] = a[i][k] / a[k][k];
+	for (j = k + 1; j < n; j++)
+	  a[i][j] = a[i][j] - a[i][k] * a[k][j];
+      }
     }
       
   }
@@ -251,16 +254,15 @@ int sscol(int n, double A[][nmax], int p[], double b[]) {
       if (p[k] != k) /*troca a linha do vetor b*/
       {
         aux = b[k];
-	      b[k] = b[p[k]];
-	      b[p[k]] = aux;
-   
+	b[k] = b[p[k]];
+	b[p[k]] = aux;
       }
    }
    for (j = 0; j < n - 1; j++) 
       for (i = j + 1; i < n; i++)
 	       b[i] = b[i] - A[i][j]*b[j];
    
-   for (j = n; j > 1; j--) {
+   for (j = n; j > -1; j--) {
     if (A[j][j] == 0)
       return -1;
    
