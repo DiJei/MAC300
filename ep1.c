17a30368cc506a1cfcc998e8a1d6b815f9796c50
dijei <djrenjiffo@terra.com.br>
2014-10-04 21:01:04 -0300

    alguams alterações nos loops e pega matriz e vetor

--- ep1.c
+++ ep1.c
@@ -21,6 +21,7 @@ Como usar: digite o nome do arquivo que tenha
 a matriz com vetor B assim como foi explicado
 na descrição do EP1.....(continua)
 */
+
 int main(int argc, char** argv) {
   
    FILE *arquivo;
@@ -28,7 +29,7 @@ int main(int argc, char** argv) {
 
    double A[nmax][nmax]; 
    double b[nmax];
-
+   int p[nmax];
 
    int i;
    int x,y;
@@ -39,23 +40,59 @@ int main(int argc, char** argv) {
    pegaVetor(arquivo, n, b);
 
   
-   /*Imprime matriz Apenas teste*/
+   /*Imprime matriz e vetor  Apenas teste*/
    for(x = 0; x < n; x++) {
    	   for(y = 0; y < n; y ++)
    	   	printf("%f ",A[x][y]);
 
        printf("\n"); 
    }
-   
    printf("\n");
    for(x = 0; x < n; x++)
       printf("%f\n",b[x]);
+   /*--------------------*/
    
+   
+   /*Acha a decomposicao LU por coluna
+   e imprime*/
+   printf("Decomposicao LU coluna:\n");
+   lucol(A,n,p);
+   for(x = 0; x < n; x++) {
+      for(y = 0; y < n; y ++)
+         printf("%f ",A[x][y]);
+       printf("\n"); 
+   }
+   /*Agora resolve por coluna e imprime*/
+   printf("Solucao Coluna: \n");
+   sscol(n,A,p,b);
+   for(x = 0; x < n; x++)
+      printf("%f\n",b[x]);
+
+    printf("\n");
+
+   /*Acha a decomposicao LU por linha
+   e imprime*/
+   printf("Decomposicao LU linha:\n");
+   lurow(A,n,p);
+   for(x = 0; x < n; x++) {
+      for(y = 0; y < n; y ++)
+         printf("%f ",A[x][y]);
+       printf("\n"); 
+   }
+   /*Agora resolve por linha e imprime*/
+   printf("Solucao linha: \n");
+   ssrow(n,A,p,b);
+   for(x = 0; x < n; x++)
+      printf("%f\n",b[x]);
+
+
    fclose(arquivo);
    return 0;
 }
-/*Corpo das funcoes*/
 
+
+
+/*Corpo das funcoes*/
 /* openSafe(char arquivo[])
 Recebe um ponteiro para FILE e devolve um outro
 se não houver arquivo nulo na entrada
@@ -75,10 +112,10 @@ FILE *openSafe(char arquivo[]) {
 Preenche a matriz
 */
 void pegaMatriz(FILE *arquivo, int n, double a[][nmax]) {
-   int i, j;
-   for (i = 0; i < n; i++){
-     for (j=0; j < n; j++)
-       fscanf(arquivo, "%lf", &a[i][j]);
+   int i,x,y;
+   for (i = 0; i < (n * n); i++){
+       fscanf(arquivo, "%d %d", &x, &y);
+       fscanf(arquivo, "%lf", &a[x][y]);
    }
 }
 /*-------------------------------------------*/
@@ -88,9 +125,10 @@ Recebe um ponteiro para tipo FILE e devolve
 um vetor do tipo   double
 */
 void pegaVetor(FILE *arquivo, int n, double intch[]) {
-   int i;
+   int i,x;
    for (i = 0; i < n; i++) { 
-      fscanf(arquivo,"%lf",&intch[i]);
+      fscanf(arquivo,"%d",&x);
+      fscanf(arquivo,"%lf",&intch[x]);
    }
 }
 /*-------------------------------------------*/
@@ -129,15 +167,15 @@ void trocaLinha(int n, int x, int m, double a[][nmax]) {
 int lucol(double a[][nmax], int n, int intch[])
 {
   int i, j, k, maxk;
-  for (k = 0; k < n; k++)
+  for (k = 0; k < (n - 1); k++)/*ate (n-1)*/
   {
     /*retorna índice do máximo*/
     maxk = 0;
-    for (i = k + 1; i < n; i++)
+    for (i = k; i < n; i++)/*antes: i = k + 1 agora i = k*/
        if (modulo(a[i][k]) > a[maxk][k])
          maxk = i;
     
-
+    
     if (a[k][maxk] = 0)
       return -1; /*É singular*/ 
     else
@@ -209,17 +247,18 @@ int lurow(double a[][nmax], int n, int intch[])
 int sscol(int n, double A[][nmax], int p[], double b[]) {
    int k,j,i;
    int aux;
-   for (k = 0; k < n; k++){
+   for (k = 0; k < (n - 1); k++){ /*ate (n-1)*/
       if (p[k] != k) /*troca a linha do vetor b*/
       {
         aux = b[k];
-	b[k] = b[p[k]];
-	b[p[k]] = aux;
+	      b[k] = b[p[k]];
+	      b[p[k]] = aux;
+   
       }
    }
    for (j = 0; j < n - 1; j++) 
       for (i = j + 1; i < n; i++)
-	b[i] = b[i] - A[i][j]*b[j];
+	       b[i] = b[i] - A[i][j]*b[j];
    
    for (j = n; j > 1; j--) {
     if (A[j][j] == 0)
